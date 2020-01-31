f26829cee51e9ad6ed1fa64428df0a43b1c29902
ak1215 <60421957+ak1215@users.noreply.github.com>
2020-01-31 08:11:48 +0100

    GUi
    giu version

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,16 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Personal Tracker");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        system.out.println("Ho");
     }
-
 
     public static void main(String[] args) {
         launch(args);
