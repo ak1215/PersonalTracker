aed50226507d5feb59330f08fcc84ac4f19e4a82
ak1215 <60421957+ak1215@users.noreply.github.com>
2020-02-10 08:09:14 +0100

    Motivation Quotes
    not working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,31 @@
 package sample;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
 public class Controller {
+    int current = 0;
+    int current2 = 0;
+    private Label dailyQuotes;
+    ArrayList<String> randomQuotes = new ArrayList<>();
+
+    public Controller(){
+        randomQuotes.add("Success is not final; failure is not fatal: It is the courage to continue that counts.");
+        randomQuotes.add("It is better to fail in originality than to succeed in imitation");
+        randomQuotes.add("The road to success and the road to failure are almost exactly the same.");
+        randomQuotes.add("Success usually comes to those who are too busy to be looking for it");
+        randomQuotes.add("Opportunities don't happen. You create them.");
+        randomQuotes.add("Don't be afraid to give up the good to go for the great.");
+    }
+
+    public void randomNr() {
+        Random random = new Random();
+        int myRand = random.nextInt(randomQuotes.size());
+        current = myRand;
+        dailyQuotes.setText(randomQuotes.get(myRand));
+        System.out.println(dailyQuotes);
+    }
 
 }
